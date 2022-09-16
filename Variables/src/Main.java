11726b303261087bde86e7a7e874a3461fa925dd
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-09-16 23:03:49 +0300

    .

---
+++ Variables/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	// reusebility
+
+	public static void main(String[] args) {
+
+		int ogrenciSayisi = 10;
+		String mesaj = "Öğrenci Sayısı";
+
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
