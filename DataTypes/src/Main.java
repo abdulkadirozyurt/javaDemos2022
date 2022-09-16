804705f2c80b5d2c065636e0244636a721e51ce1
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-09-16 23:10:34 +0300

    .

--- DataTypes/src/Main.java
+++ DataTypes/src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int sayi = 120006500;
+        int sayi = 55555555;
     	
+        double sayi2=12.5;
+        
+        
+        
+        
         
     }
 }
