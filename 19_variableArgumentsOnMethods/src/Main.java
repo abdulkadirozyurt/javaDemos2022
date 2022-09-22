27bab788678a4af23cc1960e7f9f8e86f9d2ac4d
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-09-22 18:12:54 +0300

    variableArgumentsOnMethods

--- 19_variableArgumentsOnMethods/src/Main.java
+++ 19_variableArgumentsOnMethods/src/Main.java
@@ -2,8 +2,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        
 
+        int toplam = topla(1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 6, 3, 5, 2, 3, 3, 3, 3, 2, 2, 5, 5, 6, 4, 9, 8, 7, 56456, 325, 5);
+        System.out.println(toplam);
+    }
+
+    public static int topla(int... sayilar) {
+        //  (...) variable arguments anlamina geliyor
+
+        int toplam = 0;
+        for (int sayi : sayilar) {
+            toplam += sayi;
+        }
+        return toplam;
     }
 
 }
