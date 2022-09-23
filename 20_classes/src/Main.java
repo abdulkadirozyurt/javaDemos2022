4b505c8b5f4fd461a1ab9c0b8de415192217207c
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-09-23 11:38:01 +0300

    valueAndReferenceTypes

--- 20_classes/src/Main.java
+++ 20_classes/src/Main.java
@@ -11,6 +11,30 @@ public class Main {
         customerManager.Add();
         customerManager.Remove();
         customerManager.Update();
+        
+        System.out.println("---------------------------------------------");
+
+        // value type 
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+        System.out.println(sayi2);
+
+        
+        System.out.println("---------------------------------------------");
+        
+        
+        // iki farklı dizi tanımlama şekli, ikisi de aynı anlama geliyor.
+        
+        int[] sayilar1 = new int[]{1, 2, 3};
+        int[] sayilar2 = {4, 5, 6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
+        for (int sayi : sayilar2) {
+            System.out.println(sayi);
+        }
     }
 
 
