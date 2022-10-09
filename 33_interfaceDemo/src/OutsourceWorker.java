f8a091b92d4eefd7e705d33f05768f14567d9831
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-10-09 23:05:44 +0300

    worksWithMultiImplementation

---
+++ 33_interfaceDemo/src/OutsourceWorker.java
@@ -0,0 +1,11 @@
+//firma çalışanı, dışardan çalışan
+
+public class OutsourceWorker implements IWorkable{
+
+    @Override
+    public void work() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ 33_interfaceDemo/src/Robot.java
@@ -0,0 +1,16 @@
+
+public class Robot implements IWorkable, IMaintainable {
+
+    @Override
+    public void work() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void maintenance() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
