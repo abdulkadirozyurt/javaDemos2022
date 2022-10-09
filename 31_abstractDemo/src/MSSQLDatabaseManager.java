7eed590f3df2399e7d8f48776aa2654220622a1d
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-10-09 22:06:45 +0300

    abstractDemo

---
+++ 31_abstractDemo/src/MSSQLDatabaseManager.java
@@ -0,0 +1,10 @@
+
+public class MSSQLDatabaseManager extends BaseDatabaseManager{
+
+    @Override
+    public void getData() {
+        System.out.println("Veri getirildi: MSSQL");
+        
+    }
+
+}
---
+++ 31_abstractDemo/src/MySQLDatabaseManager.java
@@ -0,0 +1,10 @@
+
+public class MySQLDatabaseManager extends BaseDatabaseManager {
+
+    @Override
+    public void getData() {
+        System.out.println("Veri Getirildi: MySQL");
+        
+    }
+
+}
