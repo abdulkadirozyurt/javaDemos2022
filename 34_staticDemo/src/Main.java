76879ea3a3dcba21e4bd2b9b7d591dbbed0449f4
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-10-09 23:46:18 +0300

    Inner Class ve Static

---
+++ 34_staticDemo/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product product1 = new Product();
+        product1.id = 1;
+        product1.name = "mouse";
+        product1.price = 10;
+        
+        ProductManager productManager = new ProductManager();
+        productManager.add(product1);
+
+    }
+
+}
